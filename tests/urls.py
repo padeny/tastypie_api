b70deb13df23332e280009ca9fbc4350c71bd030
paden <paden4j@gmail.com>
2018-10-28 20:41:46 +0800

    fix pep8

--- tests/urls.py
+++ tests/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from tastypie.api import Api  # noqa
-from tastypie.api import NamespacedApi
+from tastypie.api import NamespacedApi  # noqa: flake8
 from .api import EntryResource, Entry2Resource
 v1_api = Api(api_name='v1')
 # v1_api = NamespacedApi(api_name='v1', urlconf_namespace='tests')
