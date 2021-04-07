f9ee50fed014c809e4eb18cba814335aaed34ded
peitong.yang <peitong.yang@cue.group>
2021-04-07 16:00:56 +0800

    fix

--- tests/urls.py
+++ tests/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 from tastypie.api import Api  # noqa
 from tastypie.api import NamespacedApi  # noqa: flake8
@@ -9,7 +9,7 @@ v1_api.register(EntryResource())
 v1_api.register(Entry2Resource())
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     # path('api/', include((v1_api.urls, 'tests'), namespace='tests')),
     path('api/', include(v1_api.urls)),
 ]
